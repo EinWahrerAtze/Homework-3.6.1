ab69ca37fe6c6d5e750664124b79f4148a8617c6
EinWahrerAtze <106547632+EinWahrerAtze@users.noreply.github.com>
2022-07-05 23:41:22 +0300

    Delete Math.h

--- Math.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-double addition(double a, double b);
-double subtraction(double a, double b);
-double muliplication(double a, double b);
-double division(double a, double b);
-double power(double a, double b);
